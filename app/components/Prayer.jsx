06bd422b9c8975c85cb57ceade64106d1a2c5892
Mohamed-gp <mohamedterba6@gmail.com>
2023-11-30 21:09:32 +0100

    completing the app it need only timer and get ready

--- app/components/Prayer.jsx
+++ app/components/Prayer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Prayer = ({prayerTime}) => {
+const Prayer = ({prayerTime,prayerName,photoURL}) => {
   return (
     <div className="lg:w-[15%] w-[60%] mx-auto  bg-white pb-7 rounded-xl overflow-hidden">
             <div className="img  w-full bg-bottom bg-no-repeat " >
-              <img src="https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921" alt="" className="max-h-56 w-full" />
+              <img src={`${photoURL}`} alt="" className="max-h-56 w-full" />
             </div>
             <div className="p-6 text-xl ">
 
-              <p className="text-black font-bold mb-2">الفجر</p>
+              <p className="text-black font-bold mb-2">{prayerName}</p>
               <p className="text-4xl">{prayerTime}</p>
             </div>
           </div>
--- app/page.jsx
+++ app/page.jsx
@@ -4,38 +4,48 @@ import Prayer from "./components/Prayer"
 
 export default function Home() {
   // because api need english word and the page is arabic
-  const [currentCity, setcurrentCity] = useState("احمر العين")
+  const cities = [
+  {arName:"العفرون"
+  ,enName:"El Affroun"},
+  {arName:"اميزور"
+  ,enName:"Amizour"},
+  {arName:"حجوط"
+  ,enName:"Hadjout"}
+];
+  const [currentCity, setcurrentCity] = useState(cities[0])
   // to talk with api
-  const [currentCityAPI, setcurrentCityAPI] = useState("Ahmar El Ain")
 
   const [apiData, setapiData] = useState(null)
-  const [timing,settiming] = useState(null)
-  const MONTHS = ["جانفي","فيفري","مارس","افريل","ماي","جوان","جويلية","اوت","سمبتمبر","اكتوبر","نوفمبر","ديسمبر"]
-  const [month,setmonth] = useState(null)
+  const [timing, settiming] = useState(null)
+  const MONTHS = ["جانفي", "فيفري", "مارس", "افريل", "ماي", "جوان", "جويلية", "اوت", "سمبتمبر", "اكتوبر", "نوفمبر", "ديسمبر"]
+  const [date, setdate] = useState(null)
+  const PRAYERSNAMES = [["الفجر","Fajr","https://wepik.com/api/image/ai/9a07bb90-1edc-410f-a29a-d260a7751acf"],["الظهر","Dhuhr","https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921"],["العصر","Asr","https://wepik.com/api/image/ai/9a07baa7-b49b-4f6b-99fb-2d2b908800c2"],["المغرب","Maghrib","https://wepik.com/api/image/ai/9a07bbe3-4dd1-43b4-942e-1b2597d4e1b5"],["العشاء","Isha","https://wepik.com/api/image/ai/9a07bc25-1200-4873-8743-1c370e9eff4d"]]
+
 
 
 
 
   useEffect(() => {
-    fetch(`http://api.aladhan.com/v1/timingsByCity?city=${currentCityAPI}&country=Algeria&method=2`)
-    .then((res) => res.json())
-    .then((res) => setapiData(res))
+    fetch(`http://api.aladhan.com/v1/timingsByCity?city=${currentCity.enName}&country=Algeria&method=2`)
+      .then((res) => res.json())
+      .then((res) => setapiData(res))
 
-  }, [currentCityAPI])
+  }, [currentCity.enName])
 
 
   useEffect(() => {
     if (apiData != null) {
       console.log(apiData)
+      setdate(apiData.data.date.gregorian)
       settiming(apiData.data.timings)
     }
   }, [apiData])
 
 
   // changeing city state
-  const changeCity = (city) => {
-    setcurrentCity(city)
-    city == "احمر العين" ? setcurrentCityAPI("Ahmar El Ain") : setcurrentCityAPI("Amizour")
+  const changeCity = (e) => {
+    const selectedCity = cities.find(city => city.arName == e)
+    setcurrentCity(selectedCity)
   }
   return (
     <>
@@ -43,8 +53,11 @@ export default function Home() {
 
         <div className="  flex gap-y-5 justify-evenly sm:flex-row flex-col text-center sm:text-right text-white mt-8 font-bold ">
           <div className="flex flex-col gap-2 text-2xl sm::text-4xl">
-            <p>نوفمبر 28 2023 | 6:03</p>
-            <p>{currentCity}</p>
+            {date != undefined ?
+              <p>{date.day} {MONTHS[date.month.number - 1]}  {date.year} | {new Date().getHours()}:{new Date().getMinutes()}</p>
+              : <></>}
+
+            <p>{currentCity.arName}</p>
           </div>
           <div className="flex flex-col gap-2 text-2xl sm::text-4xl">
             <p>متبقي حتى صلاة العشاء</p>
@@ -54,24 +67,24 @@ export default function Home() {
       </div>
       <div className="container px-4 my-10">
         <div className="flex gap-14 flex-wrap flex-col lg:flex-row lg:justify-center w-[80%] lg:w-full mx-auto">
-          {timing != undefined ? 
-          <>
-            <Prayer prayerTime={timing.Fajr}/> 
-            <Prayer prayerTime={timing.Dhuhr}/> 
-            <Prayer prayerTime={timing.Asr}/> 
-            <Prayer prayerTime={timing.Maghrib}/> 
-            <Prayer prayerTime={timing.Isha}/> 
-          </>
-          : <></>}
-          
+          {timing != undefined ?
+            <>
+            {PRAYERSNAMES.map((prayerName) => {
+              return(<Prayer prayerTime={timing[prayerName[1]]} prayerName={prayerName[0]} photoURL={prayerName[2]}/>)
+            }) }
+            </>
+            : <></>}
+
         </div>
       </div>
       <div className="container flex justify-center ">
         <div className="relative cursor-pointer mb-8">
-          <select name="" id="" className="focus:outline-none border-2 border-white bg-transparent text-white   w-60  h-12" onChange={(e) => { changeCity(e.currentTarget.selectedOptions[0].textContent) }}>
-
-            <option value="" className="text-black cursor-pointer" selected={currentCity == "الرياض"}>احمر العين</option>
-            <option value="" className="text-black cursor-pointer" selected={currentCity == "مكة المكرمة"}>اميزور</option>
+          <select name="" id="" onChange={(e) => {changeCity(e.currentTarget.selectedOptions[0].textContent)}} className="focus:outline-none border-2 border-white bg-transparent text-white   w-60  h-12" >
+            {cities.map(city => {
+              return(
+                <option value="" className="text-black cursor-pointer"  defaultValue={currentCity == city.arName}>{city.arName}</option>
+              )
+            })}
           </select>
           <span className="absolute h-8 bg-[#242424] px-2 -top-4 left-5 text-white  font-bold text-xl">
             المدينة
